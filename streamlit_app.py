465826e98228d0931d675de695cf4833ebf34180
rajasubramanian89 <136018120+rajasubramanian89@users.noreply.github.com>
2023-06-11 13:04:09 +0530

    Update streamlit_app.py
    fav

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Bluberry Oatmeal') 
 streamlit.text('Kale, Spinach & Rocket smoothie')
 streamlit.text('Hard-Boiled Free Range Egg')
+streamlit.text('Avocado toast')
